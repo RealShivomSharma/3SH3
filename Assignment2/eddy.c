a797e78efa599aa31cfa8c08fc624fcdba2902bf
Eddy <eddy.che.su@gmail.com>
2023-03-30 20:48:04 -0600

    i just wanna die

--- Assignment2/eddy.c
+++ Assignment2/eddy.c
@@ -3,6 +3,7 @@
 #include <string.h> /*For memcpy function*/
 #include <fcntl.h> /*For file descriptors*/
 #include <stdlib.h> /*For file descriptors*/
+#include <stdint.h>
 /*
 Shivom Sharma 400332395
 Eddy Su 400263717
@@ -28,32 +29,28 @@ Assignment 3
 signed char *mmapfptr;
 int intArray[MEMORY_SIZE];
 
-int isEmpty(TLB *T);
+/*int isEmpty(TLB *T);
 int isFull(TLB *T);
 void TLB_init(TLB *T);
 void search_TLB(int *PAGE_NUM, TLB *T);
 void TLB_Add(int *PAGE_NUM, TLB *T);
-void TLB_Update(void);
-struct {
+void TLB_Update(void);*/
+typedef struct {
     int PAGE_NUM;
     int FRAME_NUM;
 }TLBEntry;
-struct {
+/*struct {
     int rear;
     int front;
     int count;
     TLBEntry entries[MAX_ENTRIES];
 }TLB;
 
-//Our
-struct {
 
-}PhysicalMemory;
-
-void search_TLB(int *PAGE_NUM, TLB *T) {
+void search_TLB(int *PAGE_NUM, TLBEntry *T) {
     for (int i =0; i < MAX_ENTRIES; i++) {
-        if (PAGE_NUM = TLB[i].PAGE_NUM) {
-            FRAME_NUM = TLB[i].PAGE_NUM;
+        if (PAGE_NUM = TLB.PAGE_NUM) {
+            FRAME_NUM = TLB.PAGE_NUM;
             TLB_STATUS = HIT;
         }
         else {
@@ -85,18 +82,28 @@ void TLB_Init(TLB *T) {
     T->front = -1;
     T->rear = -1;
     T->count = 0;
-}
+}*/
+
+
 
 int main() {
-    int page_table[PAGES] = {-1};
     char buff[BUFFER_SIZE];
     int PAGE_NUM = 0;
     int PAGE_OFFSET = 0;
     int Physical_address = 0;
     int frame_number = 0;
     int TLB_STATUS = 0;
-    TLB TLB;
-    TLB_INIT(TLB);
+    int TLB_total_hits = 0;
+    int TLB_total_miss = 0;
+    int page_faults_total_miss = 0;
+    int total_addresses = 0 ;
+    int logical_address;
+    int value = 0;
+
+    uint16_t *page_table = calloc(PAGE_TABLE, sizeof(uint16_t));
+    TLBEntry *TLB = NULL;
+    TLB = calloc(MAX_ENTRIES, sizeof(TLBEntry));
+    int tlb_index = 0;
 
     //opening addresses file
     FILE *fptr = fopen("addresses.txt", "r"); 
@@ -118,23 +125,29 @@ int main() {
         printf("BACKING_STORE.bin does not exist or cannot be opened.");
         return 0;
     }
+    uint8_t *mmapfptr = mmap(NULL, 1 << 16, PROT_READ, MAP_PRIVATE, mmapfile_fd, 0);
 
     while (fgets(buff, BUFFER_SIZE, fptr) != NULL) {
-        PAGE_NUM = atoi(buff) >> OFFSET_BITS;
-        for (int i = 0; i < MAX_ENTRIES; i++) {
-            if (PAGE_NUM == TLB[i].PAGE_NUM) {
-                FRAME_NUM = TLB[i].FRAME_NUM;
+        total_addresses++;
+        logical_address = atoi(buff);
+        PAGE_NUM = (logical_address >> OFFSET_BITS) & 0xff;
+        PAGE_OFFSET = atoi(buff) & 0xff;
+        Physical_address = (frame_number << OFFSET_BITS) | PAGE_OFFSET;
+
+        int index;
+        for (index = 0; index < MAX_ENTRIES; index++) {
+            if (PAGE_NUM == TLB[index].PAGE_NUM) {
+                frame_number = TLB[index].FRAME_NUM;
                 TLB_STATUS = HIT;
-            }
-            else {
-                TLB_STATUS = MISS;
+                TLB_total_hits++;
+                break;
             } 
         }
-        if (TLB_STATUS = MISS) {
-            FRAME_NUM = page_table[PAGE_NUM];
-            if (FRAME_NUM == -1) {
+        if (index == MAX_ENTRIES) {
+            frame_number = page_table[PAGE_NUM];
+            if (frame_number == 0) {
                 //Page Fault
-                mmapfptr = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, mmapfile_fd, 0);
+                page_faults_total_miss++;
                 for (int i = 0; i < MEMORY_SIZE; i++) {
                     memcpy(intArray + i, mmapfptr + 4*i, INT_SIZE);
                 }
@@ -142,12 +155,12 @@ int main() {
             }
         }
         
-
-        PAGE_OFFSET = atoi(buff) & OFFSET_MASK;
-        Physical_address = (frame_number << OFFSET_BITS) | PAGE_OFFSET;
+        fprintf(output, "Virtual address: %d Physical address = %d Value=%d", logical_address, Physical_address, value);
 
     }
+    fprintf(output, "Total addresses = %d\nPage faults = %d\nTLB Hits = %d", total_addresses, page_faults_total_miss,TLB_total_hits);
     fclose(fptr);
+    fclose(output);
     free(TLB);
     return 0;
 }
