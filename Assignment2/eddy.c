9446b6040cdd49f4dbabfb060b2fce7e08a12dca
Eddy <eddy.che.su@gmail.com>
2023-03-30 21:01:20 -0600

    kill me now

--- Assignment2/eddy.c
+++ Assignment2/eddy.c
@@ -29,69 +29,19 @@ Assignment 3
 signed char *mmapfptr;
 int intArray[MEMORY_SIZE];
 
-/*int isEmpty(TLB *T);
-int isFull(TLB *T);
-void TLB_init(TLB *T);
-void search_TLB(int *PAGE_NUM, TLB *T);
-void TLB_Add(int *PAGE_NUM, TLB *T);
-void TLB_Update(void);*/
 typedef struct {
     int PAGE_NUM;
     int FRAME_NUM;
 }TLBEntry;
-/*struct {
-    int rear;
-    int front;
-    int count;
-    TLBEntry entries[MAX_ENTRIES];
-}TLB;
-
-
-void search_TLB(int *PAGE_NUM, TLBEntry *T) {
-    for (int i =0; i < MAX_ENTRIES; i++) {
-        if (PAGE_NUM = TLB.PAGE_NUM) {
-            FRAME_NUM = TLB.PAGE_NUM;
-            TLB_STATUS = HIT;
-        }
-        else {
-            TLB_STATUS = MISS;
-        }
-
-    }
-    if (TLB_STATUS == MISS) {
-        FRAME_NUM = page_table[PAGE_NUM];
-        if(FRAME_NUM == -1) {
-            // Page Fault
-        }
-    }
-}
-    
-void TLB_Add(int *PAGE_NUM, int *FRAME_NUM, TLB *TLB) { 
-    if (TLB->count == MAX_ENTRIES) {
-        TLB->entries[front].PAGE_NUM = PAGE_NUM;
-        TLB->entries[front].FRAME_NUM = FRAME_NUM;
-    }
-     
-}
-void TLB_Update() {
-
-}
-
-void TLB_Init(TLB *T) {
-    T->entries = {0};
-    T->front = -1;
-    T->rear = -1;
-    T->count = 0;
-}*/
 
 
 
 int main() {
     char buff[BUFFER_SIZE];
     int PAGE_NUM = 0;
+    int FRAME_NUM = 0;
     int PAGE_OFFSET = 0;
     int Physical_address = 0;
-    int frame_number = 0;
     int TLB_STATUS = 0;
     int TLB_total_hits = 0;
     int TLB_total_miss = 0;
@@ -103,7 +53,7 @@ int main() {
     uint16_t *page_table = calloc(PAGE_TABLE, sizeof(uint16_t));
     TLBEntry *TLB = NULL;
     TLB = calloc(MAX_ENTRIES, sizeof(TLBEntry));
-    int tlb_index = 0;
+    int TLB_index = 0;
 
     //opening addresses file
     FILE *fptr = fopen("addresses.txt", "r"); 
@@ -113,7 +63,7 @@ int main() {
     }
 
     //opening output file
-    FILE *output = fopen("output.txt", "r"); 
+    FILE *output = fopen("output_test.txt", "w"); 
     if (output == NULL){
         printf("output.txt does not exist or cannot be opened.");
         return 0;
@@ -132,30 +82,33 @@ int main() {
         logical_address = atoi(buff);
         PAGE_NUM = (logical_address >> OFFSET_BITS) & 0xff;
         PAGE_OFFSET = atoi(buff) & 0xff;
-        Physical_address = (frame_number << OFFSET_BITS) | PAGE_OFFSET;
+        Physical_address = (FRAME_NUM << OFFSET_BITS) | PAGE_OFFSET;
 
         int index;
         for (index = 0; index < MAX_ENTRIES; index++) {
             if (PAGE_NUM == TLB[index].PAGE_NUM) {
-                frame_number = TLB[index].FRAME_NUM;
+                FRAME_NUM = TLB[index].FRAME_NUM;
                 TLB_STATUS = HIT;
                 TLB_total_hits++;
                 break;
             } 
         }
         if (index == MAX_ENTRIES) {
-            frame_number = page_table[PAGE_NUM];
-            if (frame_number == 0) {
+            TLB_total_miss++;
+            FRAME_NUM = page_table[PAGE_NUM];
+            if (FRAME_NUM == 0) {
                 //Page Fault
                 page_faults_total_miss++;
-                for (int i = 0; i < MEMORY_SIZE; i++) {
-                    memcpy(intArray + i, mmapfptr + 4*i, INT_SIZE);
-                }
+                memcpy(intArray + FRAME_NUM* PAGE_SIZE, mmapfptr+PAGE_NUM* PAGE_SIZE, PAGE_SIZE);
+                page_table[PAGE_NUM] = FRAME_NUM;
                 
             }
+            TLB[TLB_index].PAGE_NUM = PAGE_NUM;
+            TLB[TLB_index].FRAME_NUM = FRAME_NUM;
+            TLB_index = ++TLB_index % MAX_ENTRIES;
         }
         
-        fprintf(output, "Virtual address: %d Physical address = %d Value=%d", logical_address, Physical_address, value);
+        fprintf(output, "Virtual address: %d Physical address = %d Value=%d\n", logical_address, Physical_address, value);
 
     }
     fprintf(output, "Total addresses = %d\nPage faults = %d\nTLB Hits = %d", total_addresses, page_faults_total_miss,TLB_total_hits);
